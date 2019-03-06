d4cc0908011cec96da2f87ec3a01db05a4a2d9d3
GrayStrider <zhoga.ivan@gmail.com>
2019-03-06 11:51:01 +1100

    before container removal

--- src/components/TransitionGroupForm/addNotification.js
+++ src/components/TransitionGroupForm/addNotification.js
@@ -2,28 +2,25 @@ import React from "react";
 
 const AddNotification = ({ addNotification }) => {
   let input;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!input.value.trim()) {
+      input.value = "";
+      addNotification("empty value!");
+      return
+    }
+
+    addNotification(input.value);
+    input.value = "";
+  }
+
   return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            input.value = "";
-            addNotification("empty value!");
-            return;
-          }
-          addNotification(input.value);
-          input.value = "";
-        }}
-      >
-        <input
-          ref={node => {
-            input = node;
-          }}
-        />
+      <form onSubmit={handleSubmit}>
+        <input ref={node => {input = node}}/>
         <button type="submit">Add Notification</button>
       </form>
-    </div>
   );
 };
 
